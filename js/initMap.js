7aee5b8967d311732b51fc86e354ada3008b2dec
bartvdbraak <bartvdbraak@gmail.com>
2019-03-22 17:54:30 +0100

    app works from index.html to load any keyword found in API

--- js/initMap.js
+++ js/initMap.js
@@ -1,4 +1,5 @@
 var base_server = "https://tsw.valutadev.com";
+var keyword_id = null;
 
 function cityProcessor(city, map) {
     var iconBase = './img/icons/';
@@ -18,16 +19,16 @@ function cityProcessor(city, map) {
     };
 
     var request = new XMLHttpRequest();
-  
-    request.open('GET', base_server+'/keywords/'+keyword_id+'/cities/'+city.id+'/sentiment/', true);
-  
+
+    request.open('GET', base_server + '/keywords/' + keyword_id + '/cities/' + city.id + '/sentiment/', true);
+
     request.onload = function () {
         // Begin accessing sentiment data per city here
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
             //console.log(city.city, data)
             var scores = [], positive = 0, neutral = 0, negative = 0;
-            
+
             //counting each data entry
             data.forEach(entry => {
                 scores.push(entry.score);
@@ -46,23 +47,22 @@ function cityProcessor(city, map) {
 
             //console.log(avg_sent, sentiment, scores, positive, neutral, negative)
             var marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(city.location.latitude, city.location.longitude),
-                  icon: icons[sentiment],
-                  map: map
-                });
-            
-            var content =  "<h2>"+city.city+ "</h2><p>Cluster: " +city.cluster+ "<br>Avg. Sentiment score: " + avg_sent.toFixed(2) + "<br>Positive count: " + positive + "<br>Neutral count: " + neutral + "<br>Negative count: " + negative + "</p>" 
+                position: new google.maps.LatLng(city.location.latitude, city.location.longitude),
+                icon: icons[sentiment],
+                map: map
+            });
+
+            var content = "<h2>" + city.city + "</h2><p>Cluster: " + city.cluster + "<br>Avg. Sentiment score: " + avg_sent.toFixed(2) + "<br>Positive count: " + positive + "<br>Neutral count: " + neutral + "<br>Negative count: " + negative + "</p>"
             var infowindow = new google.maps.InfoWindow()
-            
+
             var data = [
-                
                 {
                     y: scores,
                     name: "Boxplot Sentiment Scores",
                     domain: {
                         row: 0,
                         column: 0
-                      },
+                    },
                     type: 'box'
                 },
                 {
@@ -71,134 +71,138 @@ function cityProcessor(city, map) {
                     domain: {
                         row: 0,
                         column: 1
-                      },
+                    },
                     hoverinfo: 'all',
                     type: 'pie'
                 }
             ];
 
             var layout = {
-                title: 'Sentiment Analysis of '+city.city,
+                title: 'Sentiment Analysis of ' + city.city,
                 showlegend: true,
-                grid: {rows: 1, columns: 2}
-              };
+                grid: { rows: 1, columns: 2 }
+            };
 
-            google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){ 
-              return function() {
-                Plotly.newPlot('plot', data, layout, {showSendToCloud:true});
-                plotOpen();
-                infowindow.setContent(content);
-                infowindow.open(map,marker);
-              };
-            })(marker,content,infowindow));     
-            
-            
+            google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+                return function () {
+                    Plotly.newPlot('plot', data, layout, { showSendToCloud: true });
+                    plotOpen();
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+                };
+            })(marker, content, infowindow));
         } else {
-          console.log('error');
+            console.log('error');
         }
     }
-  
-    request.send();  
+
+    request.send();
 }
 
 function initMap() {
-  var geocoder = new google.maps.Geocoder;
-  var map = new google.maps.Map(
-      document.getElementById('map'), {
-        minZoom: 2,
-        zoom: 2.5, 
-        maxZoom: 5,
-        center: {lat: 0.0, lng: 0.0},
-        streetViewControl: false,
-        mapTypeControl: false,
-        fullscreenControl: false,
-        mapTypeId: google.maps.MapTypeId.TERRAIN,
-        styles:     [
-          {
-              "elementType": "labels",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "administrative",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "administrative.land_parcel",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "administrative.neighborhood",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "poi",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "road",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "road",
-              "elementType": "labels.icon",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "transit",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          }
-      ]    
-        
-      });
-  // var marker = new google.maps.Marker({position: {lat: 0.0, lng: 0.0}, map: map});
+    var geocoder = new google.maps.Geocoder;
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            minZoom: 2,
+            zoom: 2.5,
+            maxZoom: 5,
+            center: { lat: 0.0, lng: 0.0 },
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            styles: [
+                {
+                    "elementType": "labels",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative",
+                    "elementType": "geometry",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative.land_parcel",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "administrative.neighborhood",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                }
+            ]
 
-  var request = new XMLHttpRequest();
-
-  request.open('GET', base_server+'/keywords/'+keyword_id+'/cities/', true);
-
-  request.onload = function () {
-    var data = JSON.parse(this.response);
-    if (request.status >= 200 && request.status < 400) {
-        data.forEach(city => {
-            cityProcessor(city, map);
         });
-      } else {
-        console.log('error');
-      }
-  }
+    // var marker = new google.maps.Marker({position: {lat: 0.0, lng: 0.0}, map: map});
 
-  request.send();
+    var request = new XMLHttpRequest();
+
+    request.open('GET', base_server + '/keywords/' + keyword_id + '/cities/', true);
+
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+            data.forEach(city => {
+                cityProcessor(city, map);
+            });
+        } else {
+            console.log('error');
+        }
+    }
+
+    request.send();
+}
+
+function setKeyword(id) {
+    console.log('try')
+    keyword_id = id;
+    initMap();
 }
--- js/layout.js
+++ js/layout.js
@@ -1,15 +1,42 @@
-window.onscroll = function() {myFunction()};
+function updateNewSuggestion() {
+  
+  document.getElementById("suggestion-text").innerText = document.getElementById("textInput").value;
+}
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+function topicSuggestions() {
+  var base_server = "https://tsw.valutadev.com";
+  var content = `<li><a href="#">Add new topic: <span style="font-weight: bold;" id="suggestion-text"></span><br /><span><i>To be added soon</></span></a></li>`
 
+  ul = document.getElementsByClassName('results')[0];
+  ul.insertAdjacentHTML('beforeend', content);
 
+  var request = new XMLHttpRequest();
+
+  request.open('GET', base_server + '/keywords/', true);
+
+  request.onload = function () {
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      data.forEach(keyword => {
+        var content = `<li><a href="javascript:setKeyword(`+keyword.id+`);"><span style="font-weight: bold;">` + keyword.name + `</span><br /><span>Uses the <span style="font-weight: bold;">` + keyword.cities_list + `</span> city list.</span></a></li>`;
+        ul.insertAdjacentHTML('beforeend', content);
+      });
+    } else {
+      console.log('error');
+    }
+  }
+
+  request.send();
+}
+
+topicSuggestions();
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
   } else {
-    navbar.classList.remove("sticky");
+    x.className = "topnav";
   }
 }
 
